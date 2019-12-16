74710b88570e062042bdd7dc9b58a6c22af62ade
Nathan Tipper <n.tipper@uleth.ca>
2019-12-15 20:14:19 -0700

    Adding some file structure

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<head>
-
+	  <title>WT Hockey</title>
+	  <meta charset="utf-8" >
+	  <meta name="author" content="William Tipper">
 	</head>
-	</body>
-		
+	<body>
+	  <h2>WT Hockey</h2>
+	  
 	</body>
 </html>
